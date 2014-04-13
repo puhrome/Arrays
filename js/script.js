bf6bd3e5419bad81c24b4310db0671f71548ccd3
puhrome <phirom.yim@fullsail.edu>
2014-04-13 15:28:29 -0400

    Array Setup
    Using WebStorm to assign Arrays in HTML and JS

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/**
+ * Arrays Created by Phirom Yim on 4/13/14.
+ */
+var tropicalFruit =["Pineapple", "Coconut", "Banana", "Jack Fruit"];//0,1,2,3
+tropicalFruit[1] = "Cocao"; //changing index 1
+tropicalFruit[2] = "Pineapple";
+console.log(tropicalFruit[1]);
+console.log(tropicalFruit[2]);
