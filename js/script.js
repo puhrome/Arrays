9a34193fc6b717e4d37da4b24222066e9d8915fe
puhrome <phirom.yim@fullsail.edu>
2014-04-13 15:30:14 -0400

    Added comments
    Added additional comments to index

--- js/script.js
+++ js/script.js
@@ -3,6 +3,6 @@
  */
 var tropicalFruit =["Pineapple", "Coconut", "Banana", "Jack Fruit"];//0,1,2,3
 tropicalFruit[1] = "Cocao"; //changing index 1
-tropicalFruit[2] = "Pineapple";
-console.log(tropicalFruit[1]);
-console.log(tropicalFruit[2]);
+tropicalFruit[2] = "Pineapple";//changing index 2
+console.log(tropicalFruit[1]);//print
+console.log(tropicalFruit[2]);//print
